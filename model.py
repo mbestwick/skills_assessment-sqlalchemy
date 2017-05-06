cb6afb0f62e0a902842197740cccf15e5127dd94
vagrant <vagrant@vagrant.vm>
2017-05-06 02:29:36 +0000

    Made Brand and Model classes

--- model.py
+++ model.py
@@ -17,7 +17,14 @@ class Brand(db.Model):
 
     __tablename__ = "brands"
 
-    pass
+    brand_id = db.Column(db.String(5), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    founded = db.Column(db.Integer)
+    headquarters = db.Column(db.String(50))
+    discontinued = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "<Brand brand_id: %s, name: %s>" % (self.brand_id, self.name)
 
 
 class Model(db.Model):
@@ -25,7 +32,21 @@ class Model(db.Model):
 
     __tablename__ = "models"
 
-    pass
+    model_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    year = db.Column(db.Integer, nullable=False)
+    brand_id = db.Column(db.String(5),
+                         db.ForeignKey('brands.brand_id'),
+                         nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    brands = db.relationship('Brand', backref=db.backref("models"))
+
+    def __repr__(self):
+        return "< Model model_id: %s, brand_id: %s, name: %s, year: %s>" % (
+            self.model_id,
+            self.brand_id,
+            self.name,
+            self.year)
 
 # End Part 1
 
